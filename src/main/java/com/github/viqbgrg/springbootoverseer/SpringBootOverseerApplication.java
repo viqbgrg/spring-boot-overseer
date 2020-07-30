645e37b29451ed423b7167cd4ecb4de91904c2c8
viqbgrg <viqbgrg@gmail.com>
2020-07-30 19:16:52 +0800

    云监工项目

---
+++ src/main/java/com/github/viqbgrg/springbootoverseer/SpringBootOverseerApplication.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.springbootoverseer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOverseerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOverseerApplication.class, args);
+    }
+
+}
