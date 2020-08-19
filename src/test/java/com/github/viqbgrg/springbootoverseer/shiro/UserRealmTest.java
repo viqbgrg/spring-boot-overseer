cde228acd12c59deae0d80f8bb3f4e7fc8585bbc
viqbgrg <viqbgrg@gmail.com>
2020-08-19 14:21:15 +0800

    优化代码

--- src/test/java/com/github/viqbgrg/springbootoverseer/shiro/UserRealmTest.java
+++ src/test/java/com/github/viqbgrg/springbootoverseer/shiro/UserRealmTest.java
@@ -8,7 +8,9 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.DefaultPasswordService;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.subject.Subject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,6 +22,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {UserServiceImpl.class, UserRealm.class})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserRealmTest {
 
     @MockBean
@@ -27,25 +30,31 @@ public class UserRealmTest {
 
     @Autowired
     private UserRealm userRealm;
+    private DefaultPasswordService defaultPasswordService;
+    private Subject subject;
+
+    @BeforeAll
+    void beforeAll() {
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        securityManager.setRealm(userRealm);
+        SecurityUtils.setSecurityManager(securityManager);
+        subject = SecurityUtils.getSubject();
+        defaultPasswordService = new DefaultPasswordService();
+    }
 
     // 密码正确
     @Test
     void login() {
-        DefaultSecurityManager securityManager = new DefaultSecurityManager();
-        securityManager.setRealm(userRealm);
-        SecurityUtils.setSecurityManager(securityManager);
-        Subject subject = SecurityUtils.getSubject();
-        DefaultPasswordService defaultPasswordService = new DefaultPasswordService();
-        String s = defaultPasswordService.encryptPassword("123");
         UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        String s = defaultPasswordService.encryptPassword("123");
         User user = new User();
         user.setUsername("zhang");
         user.setPassword(s);
         when(userService.findUserByUsername("zhang")).thenReturn(user);
         subject.login(token);
         assertThat(subject.isAuthenticated());
+        subject.logout();
     }
-
     // todo 用户不存在
 
 
