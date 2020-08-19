22f7cb64ebda57ce0fbaeef52f2fc8fb1733f1d1
viqbgrg <viqbgrg@gmail.com>
2020-08-20 00:11:28 +0800

    userrealm

--- src/test/java/com/github/viqbgrg/springbootoverseer/shiro/UserRealmTest.java
+++ src/test/java/com/github/viqbgrg/springbootoverseer/shiro/UserRealmTest.java
@@ -4,6 +4,8 @@ import com.github.viqbgrg.springbootoverseer.entity.User;
 import com.github.viqbgrg.springbootoverseer.service.UserService;
 import com.github.viqbgrg.springbootoverseer.service.impl.UserServiceImpl;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.DefaultPasswordService;
 import org.apache.shiro.mgt.DefaultSecurityManager;
@@ -18,6 +20,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -42,9 +45,9 @@ public class UserRealmTest {
         defaultPasswordService = new DefaultPasswordService();
     }
 
-    // 密码正确
+    // 密码正确 有customer权限 没有admin权限
     @Test
-    void login() {
+    void testLogin() {
         UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
         String s = defaultPasswordService.encryptPassword("123");
         User user = new User();
@@ -52,17 +55,44 @@ public class UserRealmTest {
         user.setPassword(s);
         when(userService.findUserByUsername("zhang")).thenReturn(user);
         subject.login(token);
+        assertThat(subject.isAuthenticated()).isTrue();
+        assertThat(subject.hasRole(UserRealm.CUSTOME_ROLE)).isTrue();
+        assertThat(subject.hasRole(UserRealm.ADMIN_ROLE)).isFalse();
+        subject.logout();
+    }
+
+    @Test
+    void testLoginNoUsername() {
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang1", "123");
+        when(userService.findUserByUsername("zhang1")).thenReturn(null);
+        assertThatThrownBy(() -> {
+            subject.login(token);
+        }).isInstanceOf(UnknownAccountException.class);
+    }
+
+    @Test
+    void testLoginPasswordError() {
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "1231");
+        String s = defaultPasswordService.encryptPassword("123");
+        User user = new User();
+        user.setUsername("zhang");
+        user.setPassword(s);
+        when(userService.findUserByUsername("zhang")).thenReturn(user);
+        assertThatThrownBy(() -> {
+            subject.login(token);
+        }).isInstanceOf(IncorrectCredentialsException.class);
+    }
+
+    @Test
+    void testLoginAdmin() {
+        UsernamePasswordToken token = new UsernamePasswordToken("admin", "123");
+        String s = defaultPasswordService.encryptPassword("123");
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword(s);
+        when(userService.findUserByUsername("admin")).thenReturn(user);
+        subject.login(token);
         assertThat(subject.isAuthenticated());
         subject.logout();
     }
-    // todo 用户不存在
-
-
-    //todo 密码正确
-
-    //todo 密码错误
-
-    //todo username 为admin的账号 拥有 admin customer 的权限
-
-    // todo 其它的用户只有customer的权限
 }
