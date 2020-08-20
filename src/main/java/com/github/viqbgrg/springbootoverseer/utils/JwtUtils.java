8c209254568eb50e462831431ac87b20cf6c2321
viqbgrg <viqbgrg@gmail.com>
2020-08-20 15:20:15 +0800

    jwt verify

--- src/main/java/com/github/viqbgrg/springbootoverseer/utils/JwtUtils.java
+++ src/main/java/com/github/viqbgrg/springbootoverseer/utils/JwtUtils.java
@@ -1,8 +1,11 @@
 package com.github.viqbgrg.springbootoverseer.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Date;
 
@@ -20,6 +23,20 @@ public class JwtUtils {
         } catch (JWTCreationException exception) {
             //Invalid Signing configuration / Couldn't convert Claims.
             return null;
+        }
+    }
+
+    public static boolean verify(String token, String username, String salt) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(salt);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withClaim("username", username)
+                    .build(); //Reusable verifier instance
+            DecodedJWT jwt = verifier.verify(token);
+            return true;
+        } catch (JWTVerificationException exception) {
+            //Invalid signature/claims
+            return false;
         }
     }
 }
--- src/test/java/com/github/viqbgrg/springbootoverseer/utils/JwtUtilsTest.java
+++ src/test/java/com/github/viqbgrg/springbootoverseer/utils/JwtUtilsTest.java
@@ -5,11 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class JwtUtilsTest {
+    private String username = "xiaoming";
+    private String salt = "12345";
 
     @Test
     void sign() {
-        String username = "xiaoming";
-        String token = JwtUtils.sign("user", "12345", 10);
+        String token = JwtUtils.sign(username, salt, 10);
         assertThat(token).isNotEmpty();
+    }
+
+    @Test
+    void verify() {
+        String token = JwtUtils.sign(username, salt, 10);
+        assertThat(JwtUtils.verify(token, username, salt)).isTrue();
+        assertThat(JwtUtils.verify(token, username + 1, salt)).isFalse();
+        assertThat(JwtUtils.verify(token, username, salt + 1)).isFalse();
     }
 }
