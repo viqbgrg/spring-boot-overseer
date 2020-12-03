a640ee788fe8018722418b591ac7c71650f060be
viqbgrg <viqbgrg@gmail.com>
2020-12-03 11:35:48 +0800

    登陆

--- src/test/java/com/github/viqbgrg/springbootoverseer/controller/LoginControllerTest.java
+++ src/test/java/com/github/viqbgrg/springbootoverseer/controller/LoginControllerTest.java
@@ -1,32 +1,48 @@
 package com.github.viqbgrg.springbootoverseer.controller;
 
+import com.github.viqbgrg.springbootoverseer.domain.dto.UserSignInDto;
 import com.github.viqbgrg.springbootoverseer.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(LoginController.class)
-class LoginControllerTest {
+public class LoginControllerTest {
+
     @Autowired
     private MockMvc mvc;
 
     @MockBean
     private UserService userService;
 
+
     @Test
-    void signIn() throws Exception {
-        this.mvc.perform(get("/signIn").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andExpect(content().string("Honda Civic"));
+    void signIn() {
+        UserSignInDto users = new UserSignInDto();
+        users.setUsername("xiaoming");
+        users.setPassword("123456");
+        when(userService.signIn(users)).thenReturn(true);
     }
+
+    // 用户名, 密码不符合规则 验证 然后统一异常去返回前台数据
+    @Test
+    void signInValidatedTest() {
+        UserSignInDto user = new UserSignInDto();
+        user.setUsername("xiao");
+        user.setPassword("123");
+        when(userService.signIn(user)).thenReturn(true);
+    }
+    // todo 用户名已注册
+    // TODO: 2020/8/27 027 登陆成功, 前端接收 token
+    // TODO: 2020/8/27 027 用户名和密码错误
+
 
     @Test
     void login() {
     }
+
 }
