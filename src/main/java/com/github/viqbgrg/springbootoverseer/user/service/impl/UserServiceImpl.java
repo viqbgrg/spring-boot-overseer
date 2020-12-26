e83446c589df76fba99e7b7be86cfbedb0be5b14
viqbgrg <viqbgrg@gmail.com>
2020-12-26 23:59:44 +0800

    service

--- src/main/java/com/github/viqbgrg/springbootoverseer/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/github/viqbgrg/springbootoverseer/user/service/impl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.github.viqbgrg.springbootoverseer.user.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.viqbgrg.springbootoverseer.exception.UsernameExistException;
 import com.github.viqbgrg.springbootoverseer.user.entity.User;
 import com.github.viqbgrg.springbootoverseer.user.mapper.UserMapper;
 import com.github.viqbgrg.springbootoverseer.user.service.IUserService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -23,7 +27,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     }
 
     @Override
-    public boolean signIn(User users1) {
-        return false;
+    public boolean signIn(User user) {
+        String username = user.getUsername();
+        List<User> users = this.baseMapper.selectList(new LambdaQueryWrapper<User>().eq(User::getUsername, username));
+        if (!users.isEmpty()) {
+            throw new UsernameExistException(username);
+        }
+        return this.save(user);
     }
 }
