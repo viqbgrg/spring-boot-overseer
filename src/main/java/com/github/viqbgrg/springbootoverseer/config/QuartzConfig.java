bf8a0163466694c908db5b3444e158596798ea76
viqbgrg <viqbgrg@gmail.com>
2021-04-07 11:21:02 +0800

    删除无效的

--- src/main/java/com/github/viqbgrg/springbootoverseer/config/QuartzConfig.java
+++ src/main/java/com/github/viqbgrg/springbootoverseer/config/QuartzConfig.java
@@ -12,21 +12,21 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 
 @Configuration
 public class QuartzConfig {
-    @Bean
-    public JobDetail jobDetail() {
-        return JobBuilder.newJob().ofType(SampleJob.class)
-                .storeDurably()
-                .withIdentity("job1", "group1")
-                .withDescription("Invoke Sample Job service...")
-                .build();
-    }
-
-    @Bean
-    public Trigger trigger(JobDetail job) {
-        return TriggerBuilder.newTrigger().forJob(job)
-                .withIdentity("trigger1", "group1")
-                .withDescription("Sample trigger")
-                .withSchedule(simpleSchedule().repeatForever().withIntervalInMinutes(5))
-                .build();
-    }
+//    @Bean
+//    public JobDetail jobDetail() {
+//        return JobBuilder.newJob().ofType(SampleJob.class)
+//                .storeDurably()
+//                .withIdentity("job1", "group1")
+//                .withDescription("Invoke Sample Job service...")
+//                .build();
+//    }
+//
+//    @Bean
+//    public Trigger trigger(JobDetail job) {
+//        return TriggerBuilder.newTrigger().forJob(job)
+//                .withIdentity("trigger1", "group1")
+//                .withDescription("Sample trigger")
+//                .withSchedule(simpleSchedule().repeatForever().withIntervalInMinutes(5))
+//                .build();
+//    }
 }
--- src/main/java/com/github/viqbgrg/springbootoverseer/domain/dto/UserLoginDto.java
+++ src/main/java/com/github/viqbgrg/springbootoverseer/domain/dto/UserLoginDto.java
@@ -1,5 +1,6 @@
 package com.github.viqbgrg.springbootoverseer.domain.dto;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -7,9 +8,10 @@ import lombok.Data;
  * @author bing
  */
 @Data
+@ApiModel(value = "LoginParams", description="登陆实体类")
 public class UserLoginDto {
-    @ApiModelProperty(value = "账号")
+    @ApiModelProperty(value = "账号", required = true)
     private String username;
-    @ApiModelProperty(value = "密码")
+    @ApiModelProperty(value = "密码", required = true)
     private String password;
 }
