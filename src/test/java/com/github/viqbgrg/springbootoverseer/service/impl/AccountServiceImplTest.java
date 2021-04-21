6b12561ad044792c80f248f2d739c61905da5350
viqbgrg <viqbgrg@gmail.com>
2021-04-21 15:37:25 +0800

    mock

--- src/test/java/com/github/viqbgrg/springbootoverseer/service/impl/AccountServiceImplTest.java
+++ src/test/java/com/github/viqbgrg/springbootoverseer/service/impl/AccountServiceImplTest.java
@@ -2,10 +2,13 @@ package com.github.viqbgrg.springbootoverseer.service.impl;
 
 import com.github.viqbgrg.springbootoverseer.service.IAccountService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class AccountServiceImplTest {
 
     @Autowired
